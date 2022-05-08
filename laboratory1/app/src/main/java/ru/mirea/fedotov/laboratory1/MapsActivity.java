61e793243978884cc0e4a398e2f6f40d9da8486f
Vladislav <minotraur@gmail.com>
2022-05-08 14:29:49 +0300

    Done

---
+++ laboratory1/app/src/main/java/ru/mirea/fedotov/laboratory1/MapsActivity.java
@@ -0,0 +1,88 @@
+package ru.mirea.fedotov.laboratory1;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import ru.mirea.fedotov.laboratory1.databinding.ActivityMapsBinding;
+
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
+
+    private static final int REQUEST_CODE = 100;
+    private boolean isWork = false;
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        int FineLocPermissionStatus = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        int CoarseLocPermissionStatus = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
+        if (FineLocPermissionStatus == PackageManager.PERMISSION_GRANTED && CoarseLocPermissionStatus == PackageManager.PERMISSION_GRANTED) {
+            isWork = true;
+        } else {
+            // Выполняется запрос к пользователь на получение необходимых разрешений
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                    REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setOnMapClickListener(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        } else
+            mMap.setMyLocationEnabled(true);
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setCompassEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+        setUpMap();
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(
+                latLng).zoom(12).build();
+        mMap.animateCamera(CameraUpdateFactory
+                .newCameraPosition(cameraPosition));
+        mMap.addMarker(new MarkerOptions().title("Где я?")
+                .snippet("Новое место").position(latLng));
+    }
+
+    private void setUpMap() {
+        // выбираем один вариант
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        LatLng mirea = new LatLng(55.670005, 37.479894);
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(
+                mirea).zoom(12).build();
+        mMap.animateCamera(CameraUpdateFactory
+                .newCameraPosition(cameraPosition));
+        mMap.addMarker(new MarkerOptions().title("МИРЭА")
+                .snippet("Крупнейший политехнический ВУЗ").position(mirea));
+    }
+}
